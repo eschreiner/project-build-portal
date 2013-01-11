e27492466b75e32954b347d5bacacdc54a504f52
Dr. Erich W. Schreiner <eschreiner@yahoo.com>
2013-01-11 10:51:33 +0100

    database configuration and dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -5,14 +5,21 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "pip"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appVersion      = "0.1-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "postgresql" % "postgresql" % "9.1-901.jdbc4",
+      "org.squeryl" % "squeryl_2.9.1" % "0.9.5-2",
+	  "com.typesafe.akka" % "akka-actor" % "2.0.4",
+	  
+	  "org.scalatest" %% "scalatest" % "1.8" % "test",
+	  "org.specs2" %% "specs2" % "1.12.3" % "test"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+		resolvers += "SpringSource 1" at "http://repository.springsource.com/maven/bundles/release",
+		resolvers += "SpringSource 2" at "http://repository.springsource.com/maven/libraries/release",
+		resolvers += "SpringSource 3" at "http://repository.springsource.com/maven/bundles/external"
     )
 
 }
