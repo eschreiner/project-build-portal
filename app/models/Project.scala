b43c08fa6d4e0423b3a9d6a431ac6c5434731683
Dr. Erich W. Schreiner <eschreiner@yahoo.com>
2013-01-13 00:58:01 +0100

    empty Project DAO added

--- app/models/Project.scala
+++ app/models/Project.scala
@@ -5,6 +5,14 @@ package models
  * @version 0.1.0.0
  * @since   0.1.0.0
  */
-case class Project(name: String, active: Boolean, admin_id: Long) extends PdpDbEntity {
+case class Project(name: String, active: Boolean, owner_id: Long) extends PdpDbEntity {
 
+}
+
+import Database.projectTable
+
+object Project extends PdpDbAccess[Project] {
+  
+  val table = projectTable
+  
 }
