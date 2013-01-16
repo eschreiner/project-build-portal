eb4b6ccbe91faa277715f428f8301f85c21b1234
Dr. Erich W. Schreiner <eschreiner@yahoo.com>
2013-01-16 07:09:44 +0100

    started to add hover controls

--- public/javascripts/pbp.js
+++ public/javascripts/pbp.js
@@ -1,3 +1,11 @@
 $().ready(function() {
 	$('input[type!=hidden].autofocus').focus();
+	$('.hidden-controls').hover(handleHiddenControls);
 });
+
+function handleHiddenControls(e){
+	$(this).children('.controls').toggle(e.type === 'mouseenter');
+	if (e.type === 'mouseleave') {
+		$('#shelfSelector').fadeOut();
+	}
+};
