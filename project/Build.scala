ce3e713bfc2d1fc8ec4988b3f3ef0595ab193031
Dr. Erich W. Schreiner <eschreiner@yahoo.com>
2013-11-18 11:28:08 +0100

    migrated to Play 2.1.0

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,6 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -8,15 +8,16 @@ object ApplicationBuild extends Build {
     val appVersion      = "0.1-SNAPSHOT"
 
     val appDependencies = Seq(
+		jdbc,
+//		cache,
       "postgresql" % "postgresql" % "9.1-901.jdbc4",
-      "org.squeryl" % "squeryl_2.9.1" % "0.9.5-2",
-	  "com.typesafe.akka" % "akka-actor" % "2.0.4",
+      "org.squeryl" %% "squeryl" % "0.9.5-6",
 	  
-	  "org.scalatest" %% "scalatest" % "1.8" % "test",
-	  "org.specs2" %% "specs2" % "1.12.3" % "test"
+	  "org.scalatest" %% "scalatest" % "1.9.2" % "test",
+	  "org.specs2" %% "specs2" % "2.3.4" % "test"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
 		resolvers += "SpringSource 1" at "http://repository.springsource.com/maven/bundles/release",
 		resolvers += "SpringSource 2" at "http://repository.springsource.com/maven/libraries/release",
 		resolvers += "SpringSource 3" at "http://repository.springsource.com/maven/bundles/external"
