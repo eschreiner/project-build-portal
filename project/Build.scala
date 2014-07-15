1154a88ee7957a9c6ca5436743b2eda8ed2cf91d
Dr. Erich W. Schreiner <eschreiner@yahoo.com>
2014-07-15 06:37:05 +0200

    started with migration to Play 2.3

--- project/Build.scala
+++ project/Build.scala
@@ -4,23 +4,23 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "pbp"
-    val appVersion      = "0.1-SNAPSHOT"
+  val appName         = "pbp"
+  val appVersion      = "0.1-SNAPSHOT"
 
-    val appDependencies = Seq(
-		jdbc,
-//		cache,
-      "postgresql" % "postgresql" % "9.1-901.jdbc4",
-      "org.squeryl" %% "squeryl" % "0.9.5-6",
-	  
-	  "org.scalatest" %% "scalatest" % "1.9.2" % "test",
-	  "org.specs2" %% "specs2" % "2.3.4" % "test"
-    )
+  val appDependencies = Seq(
+	jdbc,
+	cache,
+  "postgresql" % "postgresql" % "9.1-901.jdbc4",
+  "org.squeryl" %% "squeryl" % "0.9.5-6",
+  
+  "org.scalatest" %% "scalatest" % "2.2.0" % "test",
+  "org.specs2" %% "specs2" % "2.3.13" % "test"
+  )
 
-    val main = play.Project(appName, appVersion, appDependencies).settings(
-		resolvers += "SpringSource 1" at "http://repository.springsource.com/maven/bundles/release",
-		resolvers += "SpringSource 2" at "http://repository.springsource.com/maven/libraries/release",
-		resolvers += "SpringSource 3" at "http://repository.springsource.com/maven/bundles/external"
-    )
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+	resolvers += "SpringSource 1" at "http://repository.springsource.com/maven/bundles/release",
+	resolvers += "SpringSource 2" at "http://repository.springsource.com/maven/libraries/release",
+	resolvers += "SpringSource 3" at "http://repository.springsource.com/maven/bundles/external"
+  )
 
 }
